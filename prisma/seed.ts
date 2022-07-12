a232f99b410f8416ee7bf9782623a12021a4c11e
rebspen <rebecca.spence@radically.digital>
2022-07-12 15:36:14 +0100

    fixed spelling mistake and added a bigger seed file.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,17 +3,33 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.product.create({
-    data: {
-      name: "Nivea",
-      ingreedients: ["ex1", "ex2", "ex3"],
-    },
+  await prisma.product.createMany({
+    data: [
+      {
+        name: "fexofenadine hydrochloride",
+        ingredients: [
+          "fexofenadine hydrochloride",
+          "Coagulation Factor IX (Recombinant), Fc Fusion Protein",
+        ],
+      },
+      {
+        name: "ALPROLIX",
+        ingredients: ["Coagulation Factor IX (Recombinant), Fc Fusion Protein"],
+      },
+      {
+        name: "METFORMIN HYDROCHLORIDE",
+        ingredients: ["METFORMIN HYDROCHLORIDE"],
+      },
+      { name: "Baclofen", ingredients: ["Baclofen"] },
+      { name: "Nadolol", ingredients: ["Nadolol"] },
+    ],
   });
 }
 
-main().catch((e) => {
-    throw e
+main()
+  .catch((e) => {
+    throw e;
   })
   .finally(async () => {
-    await prisma.$disconnect()
-  })
+    await prisma.$disconnect();
+  });
